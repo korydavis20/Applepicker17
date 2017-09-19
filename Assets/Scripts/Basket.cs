5dafd5f3c462e037bfc73e6483b3b584d2fb283a
Kory Davis <ked130@scarletmail.rutgers.edu>
2017-09-19 16:50:51 -0400

    Step 3 - Scripting
    Added most of the scripts for the objects in apple picker. So far, I've
    gotten the AppleTree to change directions and drop apples. I haven't
    incorporated the basket and trigger collisions into the game just yet.
    
    It was somewhat difficult to follow the code in the text book due to how
    far he spaces it out. While I do like how it's shown iteration by
    iteration, I don't like that it doesn't show the final product of the
    script at the end.
    
    Due to this, I accidentally had some functions in apple that belonged to
    apple tree causing a half hour of confusion.

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Get the current screen position of the mouse from input
+
+	void Update () {
+		// the camera's z position sets the how far to push the mouse into 3D
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+
+		//convert the point from 2D screen space into 3D game world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+		//move the x position of this basket to the x position of the mouse
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
+
+	}
+
+
+
+}
