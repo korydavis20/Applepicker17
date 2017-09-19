0d754b739567707c6cb56420a1db2f1250abd079
Kory Davis <ked130@scarletmail.rutgers.edu>
2017-09-19 19:08:35 -0400

    Step 5 - Catching Apples
    Added code to the basket script to allow for on collisions with apples
    for the apple object to be removed.

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -9,15 +9,19 @@ public class Basket : MonoBehaviour {
 		
 	}
 	
-	// Get the current screen position of the mouse from input
+
 
 	void Update () {
+		
+		// Get the current screen position of the mouse from input
+		Vector3 mousePos2D = Input.mousePosition; //1
+
 		// the camera's z position sets the how far to push the mouse into 3D
-		Vector3 mousePos2D = Input.mousePosition;
-		mousePos2D.z = -Camera.main.transform.position.z;
+		mousePos2D.z = -Camera.main.transform.position.z; //2
 
 		//convert the point from 2D screen space into 3D game world space
-		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D); //3
+
 		//move the x position of this basket to the x position of the mouse
 		Vector3 pos = this.transform.position;
 		pos.x = mousePos3D.x;
@@ -25,6 +29,12 @@ public class Basket : MonoBehaviour {
 
 	}
 
-
+	void OnCollisionEnter(Collision coll){
+		//find out what hit this basket
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.tag == "Apple") {
+			Destroy (collidedWith);
+		}
+	}
 
 }
